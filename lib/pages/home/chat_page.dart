065d1d68e03cbcabd96b44b36e5dfb4dea99b500
aprildapeter <peterpsd7@gmail.com>
2023-09-23 11:20:01 +0700

    -

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -6,8 +6,68 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('chat', style: whiteTextStyle,),
+
+    Widget header(){
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        title: Text('Message Support', style: whiteTextStyle.copyWith(
+          fontSize: 18
+        ),),
+        centerTitle: true,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    Widget content(){
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: backgroundColor3,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/icon_headset.png', width: 80,),
+              SizedBox(height: 20,),
+              Text('Opss no message yet?', style: whiteTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: medium
+              ),),
+              SizedBox(height: 5,),
+              Text('You have never done a transaction', style: greyTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: medium
+              ),),
+              SizedBox(height: 20,),
+              Container(
+                height: 44,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: primaryColor,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 10
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)
+                    )
+                  ),
+                  onPressed: (){}, child: Text('Explore Store', style: whiteTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium
+                ),)),
+              )
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+        content(),
+      ],
     );
   }
 }
